50d03a6b2180815fb58d6d798ecb9d0222ff1ac3
ivanturnovski <ivanturnovski@hotmail.com>
2022-01-30 22:11:03 +0100

    [Latest Changes Homepage] + Style + JS + Delete Images

--- assets/be.js
+++ assets/be.js
@@ -30,15 +30,88 @@ $(window).on('load', function () {
 	if ($('.recent-products-slider').length) {
 	  applyRecentSlider(".recent-products-slider");
 	}
-	if ($('.categories-products-holder').length) {
-	  applyCategoriesProductSlider(".categories-products-holder");
-	}
 	if ($('.branding-slider').length) {
 	  applyBrandingSlider(".branding-slider");
 	}
   });
   
   $(document).ready(function () {
+	var numberOfItems = $('.categories-products-holder .categories-list-holder').length; 
+	var limitPerPage = 6; // Limit of items per each page
+	$('.categories-products-holder .categories-list-holder:gt(' + (limitPerPage - 1) + ')').hide();
+	var totalPages = Math.round(numberOfItems / limitPerPage); // Get number of pages
+	$(".pagination").append("<li class='current-page active'><a href='javascript:void(0)'>" + 1 + "</a></li>");
+  
+	// Loop to insert page number for each sets of items equal to page limit (e.g., limit of 4 with 20 total items = insert 5 pages)
+	for (var i = 2; i <= totalPages; i++) {
+	  $(".pagination").append("<li class='current-page'><a href='javascript:void(0)'>" + i + "</a></li>"); // Insert page number into pagination tabs
+	}
+  
+	// Add next button after all the page numbers  
+	$(".pagination").append("<li id='next-page'><a href='javascript:void(0)' aria-label=Next><span aria-hidden=true><i class='fas fa-arrow-right'></i></span></a></li>");
+  
+	// Function that displays new items based on page number that was clicked
+	$(".pagination li.current-page").on("click", function() {
+	  // Check if page number that was clicked on is the current page that is being displayed
+	  if ($(this).hasClass('active')) {
+		return false;
+	  } else {
+		var currentPage = $(this).index();
+		$(".pagination li").removeClass('active');
+		$(this).addClass('active');
+		$(".categories-products-holder .categories-list-holder").hide();
+		var grandTotal = limitPerPage * currentPage;
+  
+		// Loop through total items, selecting a new set of items based on page number
+		for (var i = grandTotal - limitPerPage; i < grandTotal; i++) {
+		  $(".categories-products-holder .categories-list-holder:eq(" + i + ")").show();
+		}
+	  }
+  
+	});
+  
+	// Function to navigate to the next page when users click on the next-page id (next page button)
+	$("#next-page").on("click", function() {
+	  var currentPage = $(".pagination li.active").index(); // Identify the current active page
+	  // Check to make sure that navigating to the next page will not exceed the total number of pages
+	  if (currentPage === totalPages) {
+		return false; // Return false (i.e., cannot navigate any further, since it would exceed the maximum number of pages)
+	  } else {
+		currentPage++; // Increment the page by one
+		$(".pagination li").removeClass('active'); // Remove the 'active' class status from the current page
+		$(".categories-products-holder .categories-list-holder").hide(); // Hide all items in the pagination loop
+		var grandTotal = limitPerPage * currentPage; // Get the total number of items up to the page that was selected
+  
+		// Loop through total items, selecting a new set of items based on page number
+		for (var i = grandTotal - limitPerPage; i < grandTotal; i++) {
+		  $(".categories-products-holder .categories-list-holder:eq(" + i + ")").show(); // Show items from the new page that was selected
+		}
+  
+		$(".pagination li.current-page:eq(" + (currentPage - 1) + ")").addClass('active'); // Make new page number the 'active' page
+	  }
+	});
+  
+	// Function to navigate to the previous page when users click on the previous-page id (previous page button)
+	$("#previous-page").on("click", function() {
+	  var currentPage = $(".pagination li.active").index(); // Identify the current active page
+	  // Check to make sure that users is not on page 1 and attempting to navigating to a previous page
+	  if (currentPage === 1) {
+		return false; // Return false (i.e., cannot navigate to a previous page because the current page is page 1)
+	  } else {
+		currentPage--; // Decrement page by one
+		$(".pagination li").removeClass('active'); // Remove the 'activate' status class from the previous active page number
+		$(".categories-products-holder .categories-list-holder").hide(); // Hide all items in the pagination loop
+		var grandTotal = limitPerPage * currentPage; // Get the total number of items up to the page that was selected
+  
+		// Loop through total items, selecting a new set of items based on page number
+		for (var i = grandTotal - limitPerPage; i < grandTotal; i++) {
+		  $(".categories-products-holder .categories-list-holder:eq(" + i + ")").show(); // Show items from the new page that was selected
+		}
+  
+		$(".pagination li.current-page:eq(" + (currentPage - 1) + ")").addClass('active'); // Make new page number the 'active' page
+	  }
+	});
+  
 	// faq js
 	var acc = document.getElementsByClassName("accordion");
 	var i;
@@ -239,23 +312,11 @@ $(window).on('load', function () {
 	  arrows: true,
 	  autoplay: true,
 	  speed: 2000,
-	});
-  }
-  applyCategoriesProductSlider = function (containerSelector) {
-	$(containerSelector).slick({
-	  dots: false,
-	  infinite: true,
-	  slidesToScroll: 1,
-	  slidesPerRow: 3,
-	  rows: 2,
-	  arrows: true,
-	  autoplay: true,
-	  speed: 2000,
 	  responsive: [{
 		breakpoint: 768,
 		settings: {
-		  slidesPerRow: 2,
-		  rows: 2,
+		  dots: true,
+		  arrows: false,
 		}
 	  }]
 	});
