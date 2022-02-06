9348dd98f504359b84300b56197e2e33adf74fb6
ivanturnovski <ivanturnovski@hotmail.com>
2022-02-07 00:13:18 +0100

    Product videos, product btn styling, js + css

--- assets/be.js
+++ assets/be.js
@@ -6,9 +6,9 @@ $(window).on('load', function () {
 	if ($('.products-holder').length) {
 	  applyProductsSlider(".products-holder");
 	}
-    if ($('.solutions-slider').length && $('.solutions-slider li').length > 3) {
-      applySolutionsSlider(".solutions-slider");
-    }
+	if ($('.solutions-slider').length && $('.solutions-slider li').length > 3) {
+	  applySolutionsSlider(".solutions-slider");
+	}
 	if ($('.products-solutions-slider').length) {
 	  applyProductSolutionsSlider(".products-solutions-slider");
 	}
@@ -27,159 +27,159 @@ $(window).on('load', function () {
 	if ($('.certificates-slider-holder').length) {
 	  applyCertificatesSlider(".certificates-slider-holder");
 	}
-    if ($('.recent-products-slider').length) {
-      setTimeout(function () { applyRecentSlider(".recent-products-slider") }, 2000);
-    }
+	if ($('.recent-products-slider').length) {
+	  setTimeout(function () { applyRecentSlider(".recent-products-slider") }, 2000);
+	}
 	if ($('.branding-slider').length) {
 	  applyBrandingSlider(".branding-slider");
 	}
-	});
-	
-	$(document).ready(function () {
-	$(".cart-button").on("click", function() {
+  });
+  
+  $(document).ready(function () {
+	$(".cart-button").on("click", function () {
 	  $('.mini-cart-holder').fadeIn();
 	  $(".mini-cart-holder").addClass('active');
 	  $(".cart-overlay").addClass('open');
 	});
-	$(".cross-icon").on("click", function() {
-	  $('.mini-cart-holder').fadeOut(); 
+	$(".cross-icon").on("click", function () {
+	  $('.mini-cart-holder').fadeOut();
 	  $(".mini-cart-holder").removeClass('active');
 	  $(".cart-overlay").removeClass('open');
 	});
-	$("form .cross-icon").on("click", function() {
+	$("form .cross-icon").on("click", function () {
 	  $(".search-container").removeClass('open');
 	  $(".search-overlay").removeClass('open');
 	  $("body").removeClass("no-scroll");
 	});
-	$(".search-overlay").on("click", function() {
+	$(".search-overlay").on("click", function () {
 	  $(".search-container").removeClass('open');
 	  $(".search-overlay").removeClass('open');
 	  $("body").removeClass("no-scroll");
 	});
-	$(".cart-overlay").on("click", function() {
-	  $('.mini-cart-holder').fadeOut(); 
+	$(".cart-overlay").on("click", function () {
+	  $('.mini-cart-holder').fadeOut();
 	  $(".mini-cart-holder").removeClass('active');
 	  $(".cart-overlay").removeClass('open');
 	});
-	$(".icon").on("click", function() {
+	$(".icon").on("click", function () {
 	  $('.menu-bar-holder').fadeIn();
 	  $(".menu-bar-holder").addClass('active');
 	  $(".menu-overlay").addClass('open');
 	});
-	$(".cross-icon").on("click", function() {
-	  $('.menu-bar-holder').fadeOut(); 
+	$(".cross-icon").on("click", function () {
+	  $('.menu-bar-holder').fadeOut();
 	  $(".menu-bar-holder").removeClass('active');
 	  $(".menu-overlay").removeClass('open');
 	});
-	$(".cart-overlay").on("click", function() {
-	  $('.menu-bar-holder').fadeOut(); 
+	$(".cart-overlay").on("click", function () {
+	  $('.menu-bar-holder').fadeOut();
 	  $(".menu-bar-holder").removeClass('active');
 	  $(".menu-overlay").removeClass('open');
 	});
-	var numberOfItems = $('.categories-products-holder .categories-list-holder').length; 
+	var numberOfItems = $('.categories-products-holder .categories-list-holder').length;
 	var limitPerPage = 6; // Limit of items per each page
 	$('.categories-products-holder .categories-list-holder:gt(' + (limitPerPage - 1) + ')').hide();
 	var totalPages = Math.round(numberOfItems / limitPerPage); // Get number of pages
 	// $(".pagination").append("<li class='current-page active'><a href='javascript:void(0)'>" + 1 + "</a></li>");
-	
+  
 	// Loop to insert page number for each sets of items equal to page limit (e.g., limit of 4 with 20 total items = insert 5 pages)
 	for (var i = 2; i <= totalPages; i++) {
-	//   $(".pagination").append("<li class='current-page'><a href='javascript:void(0)'>" + i + "</a></li>"); // Insert page number into pagination tabs
+	  //   $(".pagination").append("<li class='current-page'><a href='javascript:void(0)'>" + i + "</a></li>"); // Insert page number into pagination tabs
 	}
-	
+  
 	// Add next button after all the page numbers  
 	// $(".pagination").append("<li id='next-page'><a href='javascript:void(0)' aria-label=Next><span aria-hidden=true><i class='fas fa-arrow-right'></i></span></a></li>");
-	
+  
 	// Function that displays new items based on page number that was clicked
-	$(".pagination li.current-page").on("click", function() {
+	$(".pagination li.current-page").on("click", function () {
 	  // Check if page number that was clicked on is the current page that is being displayed
 	  if ($(this).hasClass('active')) {
-	  return false;
+		return false;
 	  } else {
-	  var currentPage = $(this).index();
-	  $(".pagination li").removeClass('active');
-	  $(this).addClass('active');
-	  $(".categories-products-holder .categories-list-holder").hide();
-	  var grandTotal = limitPerPage * currentPage;
-	
-	  // Loop through total items, selecting a new set of items based on page number
-	  for (var i = grandTotal - limitPerPage; i < grandTotal; i++) {
-	  $(".categories-products-holder .categories-list-holder:eq(" + i + ")").show();
+		var currentPage = $(this).index();
+		$(".pagination li").removeClass('active');
+		$(this).addClass('active');
+		$(".categories-products-holder .categories-list-holder").hide();
+		var grandTotal = limitPerPage * currentPage;
+  
+		// Loop through total items, selecting a new set of items based on page number
+		for (var i = grandTotal - limitPerPage; i < grandTotal; i++) {
+		  $(".categories-products-holder .categories-list-holder:eq(" + i + ")").show();
+		}
 	  }
-	  }
-	
+  
 	});
-	
+  
 	// Function to navigate to the next page when users click on the next-page id (next page button)
-	$("#next-page").on("click", function() {
+	$("#next-page").on("click", function () {
 	  var currentPage = $(".pagination li.active").index(); // Identify the current active page
 	  // Check to make sure that navigating to the next page will not exceed the total number of pages
 	  if (currentPage === totalPages) {
-	  return false; // Return false (i.e., cannot navigate any further, since it would exceed the maximum number of pages)
+		return false; // Return false (i.e., cannot navigate any further, since it would exceed the maximum number of pages)
 	  } else {
-	  currentPage++; // Increment the page by one
-	  $(".pagination li").removeClass('active'); // Remove the 'active' class status from the current page
-	  $(".categories-products-holder .categories-list-holder").hide(); // Hide all items in the pagination loop
-	  var grandTotal = limitPerPage * currentPage; // Get the total number of items up to the page that was selected
-	
-	  // Loop through total items, selecting a new set of items based on page number
-	  for (var i = grandTotal - limitPerPage; i < grandTotal; i++) {
-	  $(".categories-products-holder .categories-list-holder:eq(" + i + ")").show(); // Show items from the new page that was selected
-	  }
-	
-	  $(".pagination li.current-page:eq(" + (currentPage - 1) + ")").addClass('active'); // Make new page number the 'active' page
+		currentPage++; // Increment the page by one
+		$(".pagination li").removeClass('active'); // Remove the 'active' class status from the current page
+		$(".categories-products-holder .categories-list-holder").hide(); // Hide all items in the pagination loop
+		var grandTotal = limitPerPage * currentPage; // Get the total number of items up to the page that was selected
+  
+		// Loop through total items, selecting a new set of items based on page number
+		for (var i = grandTotal - limitPerPage; i < grandTotal; i++) {
+		  $(".categories-products-holder .categories-list-holder:eq(" + i + ")").show(); // Show items from the new page that was selected
+		}
+  
+		$(".pagination li.current-page:eq(" + (currentPage - 1) + ")").addClass('active'); // Make new page number the 'active' page
 	  }
 	});
-	
+  
 	// Function to navigate to the previous page when users click on the previous-page id (previous page button)
-	$("#previous-page").on("click", function() {
+	$("#previous-page").on("click", function () {
 	  var currentPage = $(".pagination li.active").index(); // Identify the current active page
 	  // Check to make sure that users is not on page 1 and attempting to navigating to a previous page
 	  if (currentPage === 1) {
-	  return false; // Return false (i.e., cannot navigate to a previous page because the current page is page 1)
+		return false; // Return false (i.e., cannot navigate to a previous page because the current page is page 1)
 	  } else {
-	  currentPage--; // Decrement page by one
-	  $(".pagination li").removeClass('active'); // Remove the 'activate' status class from the previous active page number
-	  $(".categories-products-holder .categories-list-holder").hide(); // Hide all items in the pagination loop
-	  var grandTotal = limitPerPage * currentPage; // Get the total number of items up to the page that was selected
-	
-	  // Loop through total items, selecting a new set of items based on page number
-	  for (var i = grandTotal - limitPerPage; i < grandTotal; i++) {
-	  $(".categories-products-holder .categories-list-holder:eq(" + i + ")").show(); // Show items from the new page that was selected
-	  }
-	
-	  $(".pagination li.current-page:eq(" + (currentPage - 1) + ")").addClass('active'); // Make new page number the 'active' page
+		currentPage--; // Decrement page by one
+		$(".pagination li").removeClass('active'); // Remove the 'activate' status class from the previous active page number
+		$(".categories-products-holder .categories-list-holder").hide(); // Hide all items in the pagination loop
+		var grandTotal = limitPerPage * currentPage; // Get the total number of items up to the page that was selected
+  
+		// Loop through total items, selecting a new set of items based on page number
+		for (var i = grandTotal - limitPerPage; i < grandTotal; i++) {
+		  $(".categories-products-holder .categories-list-holder:eq(" + i + ")").show(); // Show items from the new page that was selected
+		}
+  
+		$(".pagination li.current-page:eq(" + (currentPage - 1) + ")").addClass('active'); // Make new page number the 'active' page
 	  }
 	});
-	
+  
 	// faq js
 	var acc = document.getElementsByClassName("accordion");
 	var i;
 	for (i = 0; i < acc.length; i++) {
 	  acc[i].addEventListener("click", function () {
-	  this.classList.toggle("active");
-	  var answer = this.nextElementSibling;
-	  if (answer.style.display === "block") {
-	  answer.style.display = "none";
-	  } else {
-	  answer.style.display = "block";
-	  }
+		this.classList.toggle("active");
+		var answer = this.nextElementSibling;
+		if (answer.style.display === "block") {
+		  answer.style.display = "none";
+		} else {
+		  answer.style.display = "block";
+		}
 	  });
 	}
 	var acc = document.getElementsByClassName("accordian-menu");
 	var i;
 	for (i = 0; i < acc.length; i++) {
 	  acc[i].addEventListener("click", function () {
-	  this.classList.toggle("active");
-	  $(this).next("div").slideToggle('slow');
+		this.classList.toggle("active");
+		$(this).next("div").slideToggle('slow');
 	  });
 	}
 	var acc = document.getElementsByClassName("answer");
 	var i;
 	for (i = 0; i < acc.length; i++) {
 	  acc[i].addEventListener("click", function () {
-	  this.classList.toggle("active");
-	  $(this).next("div").slideToggle('slow');
+		this.classList.toggle("active");
+		$(this).next("div").slideToggle('slow');
 	  });
 	}
 	// show on click
@@ -194,10 +194,10 @@ $(window).on('load', function () {
 	  $(".more").toggle();
 	  var more = $(".read-more-btn").text().trim();
 	  if (more === "Read More") {
-	  $('.read-more-btn').text("Read less");
+		$('.read-more-btn').text("Read less");
 	  }
 	  else {
-	  $('.read-more-btn').text("Read More");
+		$('.read-more-btn').text("Read More");
 	  }
 	});
 	// active class js
@@ -205,66 +205,66 @@ $(window).on('load', function () {
 	var btns = header.getElementsByClassName("size-quantity");
 	for (var i = 0; i < btns.length; i++) {
 	  btns[i].addEventListener("click", function () {
-	  var current = document.getElementsByClassName("active");
-	  current[0].className = current[0].className.replace(" active", "");
-	  this.className += " active";
+		var current = document.getElementsByClassName("active");
+		current[0].className = current[0].className.replace(" active", "");
+		this.className += " active";
 	  });
 	}
-	
+  
 	// increment number
 	var clicks = 1; $(".plus").click(function () {
-	 clicks++; 
-	 $(".quantity").val(clicks);
-	})
-	var clicks = 1; 
-	$(".minus").click(function () {
-	if(clicks > 1){ 
-	  clicks--; 
+	  clicks++;
 	  $(".quantity").val(clicks);
-	}
+	})
+	var clicks = 1;
+	$(".minus").click(function () {
+	  if (clicks > 1) {
+		clicks--;
+		$(".quantity").val(clicks);
+	  }
 	});
-	});
-	
-	//stars rating
-	jQuery(function ($) {
+  });
+  
+  //stars rating
+  jQuery(function ($) {
 	var count = $('.rating-stars').data('defaultValue');
 	//  $('.rating-stars > span').slice(0, count).addClass("rated");
-	  $('.rating-stars').children('span').each(function(i) {
-		$(this).addClass('rated');
-		if (i >= count) {
+	$('.rating-stars').children('span').each(function (i) {
+	  $(this).addClass('rated');
+	  if (i >= count) {
 		$(this).find(">:first-child").addClass('far fa-star');
 		$(this).find(">:first-child").removeClass('fa');
-		}
-	  });
+	  }
+	});
 	var count = $('.rating-stars-product').data('defaultValue');
 	//  $('.rating-stars-product > span').slice(0, count).addClass("rated");
-	  $('.rating-stars-product').children('span').each(function(i) {
-		$(this).addClass('rated');
-		if (i >= count) {
+	$('.rating-stars-product').children('span').each(function (i) {
+	  $(this).addClass('rated');
+	  if (i >= count) {
 		$(this).find(">:first-child").addClass('far fa-star');
 		$(this).find(">:first-child").removeClass('fa');
-		}
-	  });
+	  }
+	});
 	var count = $('.rating-stars-reviews').data('defaultValue');
 	//  $('.rating-stars-reviews > span').slice(0, count).addClass("rated");
-	  $('.rating-stars-reviews').children('span').each(function(i) {
-		$(this).addClass('rated');
-		if (i >= count) {
+	$('.rating-stars-reviews').children('span').each(function (i) {
+	  $(this).addClass('rated');
+	  if (i >= count) {
 		$(this).find(">:first-child").addClass('far fa-star');
 		$(this).find(">:first-child").removeClass('fa');
-		}
-	  });
+	  }
+	});
 	var count = $('.rating-stars-reviews-form').data('defaultValue');
 	//  $('.rating-stars-reviews-form > span').slice(0, count).addClass("rated");
-	  $('.rating-stars-reviews-form').children('span').each(function(i) {
-		$(this).addClass('rated');
-		if (i >= count) {
+	$('.rating-stars-reviews-form').children('span').each(function (i) {
+	  $(this).addClass('rated');
+	  if (i >= count) {
 		$(this).find(">:first-child").addClass('far fa-star');
 		$(this).find(">:first-child").removeClass('fa');
-		}
-	  });
+	  }
 	});
-	applyBannerSlider = function (containerSelector) {
+  });
+  applyBannerSlider = function (containerSelector) {
 	$(containerSelector).slick({
 	  dots: true,
 	  infinite: true,
@@ -274,14 +274,14 @@ $(window).on('load', function () {
 	  autoplay: true,
 	  autoplayspeed: 2000,
 	  responsive: [{
-	  breakpoint: 768,
-	  settings: {
-	  dots: false,
-	  }
+		breakpoint: 768,
+		settings: {
+		  dots: false,
+		}
 	  }]
 	});
-	}
-	applyProductsSlider = function (containerSelector) {
+  }
+  applyProductsSlider = function (containerSelector) {
 	$(containerSelector).slick({
 	  dots: false,
 	  infinite: true,
@@ -291,16 +291,16 @@ $(window).on('load', function () {
 	  autoplay: true,
 	  speed: 2000,
 	  responsive: [{
-	  breakpoint: 768,
-	  settings: {
-	  slidesToShow: 1,
-	  dots: true,
-	  arrows: false,
-	  }
+		breakpoint: 768,
+		settings: {
+		  slidesToShow: 1,
+		  dots: true,
+		  arrows: false,
+		}
 	  }]
 	});
-	}
-	applySolutionsSlider = function (containerSelector) {
+  }
+  applySolutionsSlider = function (containerSelector) {
 	$(containerSelector).slick({
 	  dots: true,
 	  infinite: true,
@@ -310,8 +310,8 @@ $(window).on('load', function () {
 	  autoplay: true,
 	  speed: 2000,
 	});
-	}
-	applyProductSolutionsSlider = function (containerSelector) {
+  }
+  applyProductSolutionsSlider = function (containerSelector) {
 	$(containerSelector).slick({
 	  dots: false,
 	  infinite: true,
@@ -321,8 +321,8 @@ $(window).on('load', function () {
 	  autoplay: true,
 	  speed: 2000,
 	});
-	}
-	applyNewLaunchesSlider = function (containerSelector) {
+  }
+  applyNewLaunchesSlider = function (containerSelector) {
 	$(containerSelector).slick({
 	  dots: true,
 	  infinite: true,
@@ -332,14 +332,14 @@ $(window).on('load', function () {
 	  autoplay: true,
 	  speed: 2000,
 	  responsive: [{
-	  breakpoint: 768,
-	  settings: {
-	  slidesToShow: 2,
-	  }
+		breakpoint: 768,
+		settings: {
+		  slidesToShow: 2,
+		}
 	  }]
 	});
-	}
-	applyBlogsSlider = function (containerSelector) {
+  }
+  applyBlogsSlider = function (containerSelector) {
 	$(containerSelector).slick({
 	  dots: false,
 	  infinite: true,
@@ -349,15 +349,15 @@ $(window).on('load', function () {
 	  autoplay: true,
 	  speed: 2000,
 	  responsive: [{
-	  breakpoint: 768,
-	  settings: {
-	  dots: true,
-	  arrows: false,
-	  }
+		breakpoint: 768,
+		settings: {
+		  dots: true,
+		  arrows: false,
+		}
 	  }]
 	});
-	}
-	applyCategoriesSlider = function (containerSelector) {
+  }
+  applyCategoriesSlider = function (containerSelector) {
 	$(containerSelector).slick({
 	  dots: false,
 	  infinite: true,
@@ -367,14 +367,14 @@ $(window).on('load', function () {
 	  autoplay: true,
 	  speed: 2000,
 	  responsive: [{
-	  breakpoint: 480,
-	  settings: {
-	  slidesToShow: 3,
-	  }
+		breakpoint: 480,
+		settings: {
+		  slidesToShow: 3,
+		}
 	  }]
 	});
-	}
-	applyProductsImageSlider = function (containerSelector) {
+  }
+  applyProductsImageSlider = function (containerSelector) {
 	$(containerSelector).slick({
 	  dots: true,
 	  infinite: true,
@@ -384,8 +384,8 @@ $(window).on('load', function () {
 	  autoplay: true,
 	  speed: 2000,
 	});
-	}
-	applyCertificatesSlider = function (containerSelector) {
+  }
+  applyCertificatesSlider = function (containerSelector) {
 	$(containerSelector).slick({
 	  dots: true,
 	  infinite: true,
@@ -395,26 +395,26 @@ $(window).on('load', function () {
 	  autoplay: true,
 	  speed: 2000,
 	});
-	}
-	applyRecentSlider = function (containerSelector) {
+  }
+  applyRecentSlider = function (containerSelector) {
 	$(containerSelector).slick({
 	  dots: false,
 	  infinite: true,
 	  slidesToScroll: 1,
-	  slidesToShow: 3,
+	  slidesToShow: 2,
 	  arrows: true,
 	  autoplay: true,
 	  speed: 2000,
 	  responsive: [{
-	  breakpoint: 768,
-	  settings: {
-	  dots: true,
-	  arrows: false,
-	  }
+		breakpoint: 768,
+		settings: {
+		  dots: true,
+		  arrows: false,
+		}
 	  }]
 	});
-	}
-	applyBrandingSlider = function (containerSelector) {
+  }
+  applyBrandingSlider = function (containerSelector) {
 	$(containerSelector).slick({
 	  dots: true,
 	  infinite: true,
@@ -424,21 +424,21 @@ $(window).on('load', function () {
 	  autoplay: true,
 	  speed: 2000,
 	  responsive: [
-	  {
-	  breakpoint: 9999,
-	  settings: "unslick"
-	  },
-	  {
-	  breakpoint: 767,
-	  settings: {
-	  slidesToScroll: 1,
-	  slidesToShow: 1,
-	  infinite: true,
-	  dots: false,
-	  dots: true,
-	  }
-	  }
+		{
+		  breakpoint: 9999,
+		  settings: "unslick"
+		},
+		{
+		  breakpoint: 767,
+		  settings: {
+			slidesToScroll: 1,
+			slidesToShow: 1,
+			infinite: true,
+			dots: false,
+			dots: true,
+		  }
+		}
 	  ]
 	});
-	}
+  }
   
